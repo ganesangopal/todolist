8938263f4c62259ec845431ec520e0a96e56a6a2
ganesangopal <siva.subramanian@krds.fr>
2018-07-21 16:58:15 +0530

    Fixed link issue

--- modules/custom/todo_custom/src/Controller/TodoCustomController.php
+++ modules/custom/todo_custom/src/Controller/TodoCustomController.php
@@ -26,7 +26,7 @@ class TodoCustomController extends ControllerBase {
       $node = Node::load($nid);
       $todo_status = $node->get('field_todo_status')->getValue();
       if (!$todo_status[0]['value']) {
-        $node->field_todo_status[] = ['value' => 1];
+        $node->field_todo_status[0] = ['value' => 1];
         $node->save();
         drupal_set_message($this->t('Todo list of @node marked as completed successfully.', ['@node' => $node->title]), 'status', TRUE);
     }
